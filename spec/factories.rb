00359751170a42075f8d23f2b76fd676812f9775
alessandrodimich <alessandrodimich@hotmail.com>
2013-12-23 22:10:07 +0100

    created microposts

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,10 +7,19 @@ FactoryGirl.define do
     user_name "dimich71"
     password "foobar"
     password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 
   factory :event do
     name "Test Event"
+    user
+  end
+
+  factory :micropost do
+    content "Lorem ipsum"
     user
   end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,43 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
 
-describe User do
-  pending "User Email should be in lower case"
-end
-
-describe User do
   it "should have a 'name' attribute" do
     user = User.new
     user.should respond_to(:first_name)
     user.should respond_to(:last_name)
-    user.should respond_to(:last_name)
+    user.should respond_to(:user_name)
+    user.should respond_to(:email)
+    user.should respond_to(:password)
   end
+
+  before do
+    @user = User.new(first_name: "Example", last_name: "User", user_name: "ExampleUser", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
+
+  subject { @user }
+
+  it { should respond_to(:admin) }
+  it { should respond_to(:microposts) }
+
+
+  describe "micropost associations" do
+
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+    end
+
+  end
+
 end
 
 
