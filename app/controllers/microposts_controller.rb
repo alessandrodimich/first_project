63c6c5fd5139828ce47185444c14ef6bc32cd88c
alessandrodimich <alessandrodimich@hotmail.com>
2013-12-30 21:50:23 +0100

    Finished first part of the microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
 
   before_action :signed_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
 
   def create
     puts "+++++++++++++++++++++++++++++#{params[:content]}"
@@ -9,11 +10,14 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
+      @feed_items = []
       render 'static_pages/home'
     end
   end
 
   def destroy
+    @micropost.destroy
+    redirect_to home_url
 
   end
 
@@ -25,6 +29,11 @@ class MicropostsController < ApplicationController
 
   def micropost_params
     params.require(:micropost).permit(:content)
+  end
+
+  def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to home_url if @micropost.nil?
   end
 
 end
